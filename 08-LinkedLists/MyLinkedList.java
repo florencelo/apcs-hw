4f0846a438fb607b63465857b096cd2e3031a619
Florence Lo <xin.lo@cslab1-19.stuy.edu>
2014-03-20 09:07:14 -0400

    update

--- 08-LinkedLists/MyLinkedList.java
+++ 08-LinkedLists/MyLinkedList.java
@@ -1,7 +1,7 @@
 public class MyLinkedList {
 
     private Node head;
-	private int listCount;
+    private int listCount;
 
     public MyLinkedList() {
 	head = null;
@@ -16,79 +16,86 @@ public class MyLinkedList {
     }
     
     /*public String toString() {
-    	Node tmp = head;
-	String s= "";
-	while(tmp != null) {
-		tmp = tmp.getNext();
-		s += " " + tmp;
-	}
-	return s;
-    }*/
+      Node tmp = head;
+      String s= "";
+      while(tmp != null) {
+      tmp = tmp.getNext();
+      s += " " + tmp;
+      }
+      return s;
+      }*/
     
     public void add(int index, String s){
     	Node temp = new Node(s);
-		Node current = head;
-		for(int i = 1; i < index && current.getNext() != null; i++)
-		{
-			current = current.getNext();
-		}
-		temp.setNext(current.getNext());
-		current.setNext(temp);
-		listCount++;
+	Node current = head;
+	for(int i = 1; i < index && current.getNext() != null; i++)
+	    {
+		current = current.getNext();
+	    }
+	temp.setNext(current.getNext());
+	current.setNext(temp);
+	listCount++;
 
     }
     public String get(int index)
-	{
-		if(index <= 0)
-			return null;
+    {
+	if(index <= 0)
+	    return null;
 		
-		Node current = head.getNext();
-		for(int i = 1; i < index; i++)
-		{
-			if(current.getNext() == null)
-				return null;
+	Node current = head.getNext();
+	for(int i = 1; i < index; i++)
+	    {
+		if(current.getNext() == null)
+		    return null;
 			
-			current = current.getNext();
-		}
-		return current.getData();
-	}
+		current = current.getNext();
+	    }
+	return current.getData();
+    }
 	
-	public boolean remove(int index)
-	{
-		if(index < 1 || index > length())
-			return false;
+    public boolean remove(int index)
+    {
+	if(index < 1 || index > size())
+	    return false;
 		
-		Node current = head;
-		for(int i = 1; i < index; i++)
-		{
-			if(current.getNext() == null)
-				return false;
+	Node current = head;
+	for(int i = 1; i < index; i++)
+	    {
+		if(current.getNext() == null)
+		    return false;
 			
-			current = current.getNext();
-		}
-		current.setNext(current.getNext().getNext());
-		listCount--; 
-		return true;
-	}
+		current = current.getNext();
+	    }
+	current.setNext(current.getNext().getNext());
+	listCount--; 
+	return true;
+    }
 	
-	public int length()
-	{
-		return listCount;
-	}
-	
-	public String toString()
-	{
-		Node current = head.getNext();
-		String output = "";
-		while(current != null)
-		{
-			output += "[" + current.getData().toString() + "]";
-			current = current.getNext();
-		}
-		return output;
-	}
+    public int size()
+    {
+	return listCount;
+    }
 
-        public static void main(String[] args){
+    /*
+Method 1: use a loop a la toString to calculate the length and return it O(n)
+Method 2: make a length instance variable add 1 to length in add routines subtract 1 from length in remove routines just returns length amortized O(k)
+
+amortized - calc use of money over time
+     */
+	
+    public String toString()
+    {
+	Node current = head.getNext();
+	String output = "";
+	while(current != null)
+	    {
+		output += "[" + current.getData().toString() + "]";
+		current = current.getNext();
+	    }
+	return output;
+    }
+
+    public static void main(String[] args){
     	MyLinkedList L = new MyLinkedList();
 
     	L.add("Sully");
